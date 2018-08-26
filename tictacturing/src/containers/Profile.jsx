aca5731c04792c756b7274a261e492345a348c2c
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-08-26 16:41:51 -0600

    Profile page styles.

--- tictacturing/src/containers/Profile.jsx
+++ tictacturing/src/containers/Profile.jsx
@@ -1,12 +1,59 @@
 import React, { Component } from 'react'
+import { Container, Name, GameListHeader, 
+    GameList, GameRecord, Column, ColumnLabels } from '../styled/Profile'
 
 class Profile extends Component {
 
+    static defaultProps = {
+        user: {
+            email: 'USER_EMAIL',
+            games: [
+                {
+                    winner: true,
+                    createdAt: '08/24/2018',
+                    id: '0001'
+                },
+                {
+                    winner: false,
+                    createdAt: '08/25/2018',
+                    id: '0002'
+                },
+                {
+                    winner: true,
+                    createdAt: '08/26/2018',
+                    id: '0003'
+                }
+            ]
+        }
+    }
+
     render() {
+        let { email } = this.props.user
         return (
-            <div>
-                <h2>Profile!</h2>
-            </div>
+            <Container>
+                <Name>
+                    {email}
+                </Name>
+                <GameList>
+                    <GameListHeader>
+                        My Games
+                    </GameListHeader>
+                    <ColumnLabels>
+                        <Column>
+                        Outcome
+                        </Column>
+                        <Column>
+                        Guess
+                        </Column>
+                        <Column>
+                        Guessed Correctly
+                        </Column>
+                        <Column>
+                        Date
+                        </Column>
+                    </ColumnLabels>
+                </GameList>
+            </Container>
         )
     }
 }
