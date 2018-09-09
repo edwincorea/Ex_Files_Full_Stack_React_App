bcd70d423ecdaaf45e0691378048fa8796140f8c
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-09-09 14:00:47 -0600

    Injecting the Relay network layer

--- tictacturing/src/index.js
+++ tictacturing/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, browserHistory } from 'react-router'
+import { Router, browserHistory, applyRouterMiddleware } from 'react-router'
 
-import Relay from 'react-relay'
+import Relay from 'react-relay/classic'
 import userRelay from 'react-router-relay'
 import { RelayNetworkLayer, urlMiddleware } from 'react-relay-network-layer'
 import { relayApiUrl } from './config/endpoints'
@@ -13,14 +13,33 @@ import Routes from './routes'
 const createHeaders = () => {
     let idToken = auth.getToken()
     if (idToken) {
-        Authorization: `Bearer ${idToken}`
+        return {
+            Authorization: `Bearer ${idToken}`
+        }
     } else {
         return {}
     }
 }
 
+Relay.injectNetworkLayer(
+    new RelayNetworkLayer([
+        urlMiddleware({
+            url: (req) => relayApiUrl,            
+        }),
+        next => req => {
+            req.headers = {
+                ...req.headers,
+                ...createHeaders()
+            }
+            return next(req)
+        },
+    ], {disableBatchQuery: true})
+)
+
 ReactDOM.render(
     <Router
+        environment={Relay.Store}
+        render={applyRouterMiddleware(userRelay)}
         history={browserHistory}
         routes={Routes}
     />,
